ac209d5ff07af32bf0ed47b777be668c5994fec6
KamilFig <figurka1234@gmail.com>
2018-01-19 11:37:53 +0100

    .

--- src/main/java/com/pracownia/spring/services/AddressServicempl.java
+++ src/main/java/com/pracownia/spring/services/AddressServicempl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
-
+@Service
 public class AddressServicempl implements  AddressService{
 
     @Autowired
